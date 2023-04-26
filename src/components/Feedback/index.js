d64438c35263ff1f690b03bd5762ec415ab4f17c
ebalu0776 <ebalu0776@gmail.com>
2023-04-26 05:04:50 +0000

    solution

---
+++ src/components/Feedback/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <div className="emojis-container">
+          <h1 className="heading">hdfhdf</h1>
+        </div>
+      </div>
+    )
+  }
+}
+export default Feedback
